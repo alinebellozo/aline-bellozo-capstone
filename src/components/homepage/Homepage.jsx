9f44e572792fbd9e2f8d8e7afac241cc69cd43d5
Aline Fernanda Bellozo <alinebellozo@gmail.com>
2022-12-05 14:41:58 -0800

    New folder: client

---
+++ src/components/homepage/Homepage.jsx
@@ -0,0 +1,30 @@
+import "./Homepage.scss";
+
+export default function Homepage() {
+  return (
+    <>
+      <section className="homepage">
+        <div className="homepage__side-menu">
+          <h1 className="">LOGO</h1>
+          <p className="">Option 1</p>
+          <p className="">Option 2</p>
+          <p className="">Option 3</p>
+          <p className="">Option 4</p>
+        </div>
+
+        <section className="homepage__carousel">
+          <div className="">JavaScript, HTML, CSS</div>
+          <div className="">Python</div>
+          <div className="">Java</div>
+          <div className="">C++</div>
+        </section>
+
+        <section className="homepage__content">
+          <h1 className="">Why study group?</h1>
+          <p className="">Explanation</p>
+          <p className="">More content</p>
+        </section>
+      </section>
+    </>
+  );
+}
